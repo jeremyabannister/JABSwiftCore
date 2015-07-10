dc45d582fbc3098958354ab42f071427d51b6438
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2015-07-10 09:31:57 -0700

    • Pushing before plane take-off to avoid a repeat of return trip from Eurotrip

---
+++ JABSwiftCore/NSDateExtension.swift
@@ -0,0 +1,15 @@
+//
+//  NSDateExtension.swift
+//  JABSwiftCore
+//
+//  Created by Jeremy Bannister on 7/7/15.
+//  Copyright (c) 2015 Jeremy Bannister. All rights reserved.
+//
+
+import Foundation
+
+public extension NSDate {
+    
+    
+    
+}
