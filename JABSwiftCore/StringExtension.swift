ab64b5e230f9361aed721c45a5549b4449fc7823
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2015-07-12 19:06:23 -0400

    • Added String.doubleFromDollarAmount()

--- JABSwiftCore/StringExtension.swift
+++ JABSwiftCore/StringExtension.swift
@@ -118,7 +118,47 @@ public extension String {
     
     
     // MARK: Conversion
-    
+    public func doubleFromDollarAmount () -> Double? {
+        
+        if isValidDollarAmount() {
+            var testSubject = self
+            var negative = false
+            
+            if count(testSubject) > 0 {
+                if testSubject[0] == "-" {
+                    negative = true
+                    testSubject = testSubject.decapitate()
+                }
+            }
+            
+            if count(testSubject) > 0 {
+                if testSubject[0] == "$" {
+                    testSubject = testSubject.decapitate()
+                } else {
+                    println("Problem in JABSwiftCore.String.doubleFromDollarAmount - The string \"\(self)\" does not begin with either \"$\" or \"-$\".")
+                    return nil
+                }
+            }
+            
+            if count(testSubject) > 0 {
+                if let doubleAmount = testSubject.toDouble() {
+                    if negative {
+                        return -doubleAmount
+                    } else {
+                        return doubleAmount
+                    }
+                } else {
+                    return nil
+                }
+            } else {
+                return 0.0
+            }
+            
+        } else {
+            println("Problem in JABSwiftCore.String.doubleFromDollarAmount - The string \"\(self)\" is not a valid dollar amount.")
+            return nil
+        }
+    }
     
     public func toDouble() -> Double? {
         return NSNumberFormatter().numberFromString(self)?.doubleValue
