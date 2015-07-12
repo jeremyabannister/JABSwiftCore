4dacbe91573909bbd9c8caf24d32945f44d8f9c6
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2015-07-12 19:07:57 -0400

    • Added DoubleExtension.swift

---
+++ JABSwiftCore/DoubleExtension.swift
@@ -0,0 +1,21 @@
+//
+//  DoubleExtension.swift
+//  JABSwiftCore
+//
+//  Created by Jeremy Bannister on 7/12/15.
+//  Copyright (c) 2015 Jeremy Bannister. All rights reserved.
+//
+
+import Foundation
+
+public extension Double {
+    
+    // MARK:
+    // MARK: Properties
+    // MARK:
+    
+    // MARK:
+    // MARK: Methods
+    // MARK:
+    
+}
