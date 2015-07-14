e2b8d964c38ec5ab77ad847bd18ac5532bf9f5e9
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2015-07-14 00:49:00 -0400

    • Added UIImage.imageTintedWithColor:

--- JABSwiftCore/UIImageExtension.swift
+++ JABSwiftCore/UIImageExtension.swift
@@ -13,4 +13,40 @@ import UIKit
 public extension UIImage {
     
     
+    public func imageTintedWithColor(color: UIColor) -> UIImage {
+        // This method is designed for use with template images, i.e. solid-coloured mask-like images.
+        return imageTintedWithColor(color, fraction: 0.0) // default to a fully tinted mask of the image.
+    }
+    
+    public func imageTintedWithColor(color: UIColor, fraction: CGFloat) -> UIImage {
+        
+        // Construct new image the same size as this one.
+        var image = UIImage()
+        if UIScreen.instancesRespondToSelector("scale") {
+            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        } else {
+            UIGraphicsBeginImageContext(size)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        
+        // Composite tint color at its own opacity.
+        color.set()
+        UIRectFill(rect)
+        
+        // Mask tint color-swatch to this image's opaque mask.
+        // We want behaviour like NSCompositeDestinationIn on Mac OS X.
+        drawInRect(rect, blendMode: kCGBlendModeDestinationIn, alpha: 1.0)
+        
+        // Finally, composite this image over the tinted mask at desired opacity.
+        if fraction > 0.0 {
+            drawInRect(rect, blendMode: kCGBlendModeSourceAtop, alpha: fraction)
+        }
+        image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+        
+    }
+    
 }
