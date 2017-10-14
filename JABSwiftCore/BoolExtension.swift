fa231f1f63a18d42e4fa30c6b6ac3d1ef1b32d0d
jeremyabannister <jeremy.a.bannister@gmail.com>
2017-10-14 01:36:01 -0700

    • Added BoolExtension.swift which defines xOr method on Bool   ( myBool.xOr(otherBool) )

---
+++ JABSwiftCore/BoolExtension.swift
@@ -0,0 +1,13 @@
+//
+//  BoolExtension.swift
+//  JABSwiftCore
+//
+//  Created by Jeremy Bannister on 10/14/17.
+//  Copyright © 2017 Jeremy Bannister. All rights reserved.
+//
+
+import Foundation
+
+public extension Bool {
+    public func xOr (_ otherBool: Bool) { return (self || otherBool) && !(self && otherBool) }
+}
