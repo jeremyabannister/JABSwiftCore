87f53ce71910df8e8a9d0f20e440d2294d3a10f2
jeremyabannister <jeremy.a.bannister@gmail.com>
2018-01-28 19:26:28 -0800

    • UIImage can now be easily read from and written to the disk (and read from bundle)

--- JABSwiftCore/StringExtension.swift
+++ JABSwiftCore/StringExtension.swift
@@ -10,169 +10,164 @@ import Foundation
 
 public extension String {
     
-    // ----------------------
-    // MARK: Subscript
-    // ----------------------
-    subscript (index: Int) -> Substring? {
-        if !self.hasIndex(index) { return nil }
-        let stringIndex = self.index(self.startIndex, offsetBy: index)
-        return self[stringIndex ... stringIndex]
-    }
-    subscript (range: CountableRange<Int>) -> Substring {
-        let lowerBound = self.ensurePosition(range.lowerBound)
-        let upperBound = self.ensurePosition(range.upperBound)
-        return self[self.index(self.startIndex, offsetBy: lowerBound) ..< self.index(self.startIndex, offsetBy: upperBound)]
-    }
-    subscript (closedRange: CountableClosedRange<Int>) -> Substring {
-        var lowerBound = self.ensurePosition(closedRange.lowerBound)
-        var upperBound = self.ensurePosition(closedRange.upperBound)
-        if lowerBound == upperBound && lowerBound == count { return "" }
-        lowerBound = self.ensureIndex(lowerBound)
-        upperBound = self.ensureIndex(upperBound)
-        return self[self.index(self.startIndex, offsetBy: lowerBound) ... self.index(self.startIndex, offsetBy: upperBound)]
-    }
-    subscript (partialRangeFrom: CountablePartialRangeFrom<Int>) -> Substring {
-        if partialRangeFrom.lowerBound >= count { return "" }
-        let lowerBound = self.ensureIndex(partialRangeFrom.lowerBound)
-        return self[self.index(self.startIndex, offsetBy: lowerBound)...]
-    }
-    subscript (partialRangeUpTo: PartialRangeUpTo<Int>) -> Substring {
-        let upperBound = self.ensurePosition(partialRangeUpTo.upperBound)
-        return self[..<self.index(self.startIndex, offsetBy: upperBound)]
-    }
-    subscript (partialRangeThrough: PartialRangeThrough<Int>) -> Substring {
-        if partialRangeThrough.upperBound < 0 { return "" }
-        let upperBound = self.ensureIndex(partialRangeThrough.upperBound)
-        return self[...self.index(self.startIndex, offsetBy: upperBound)]
-    }
-    
-    // An index marks the locations of the characters, while a position marks the locations between characters
-    public func hasIndex (_ index: Int) -> Bool { return index == self.ensureIndex(index) }
-    public func ensureIndex (_ index: Int) -> Int { return index < 0 ? 0 : (index >= count ? count - 1 : index) }
-    public func hasPosition (_ position: Int) -> Bool { return position == self.ensurePosition(position) }
-    public func ensurePosition (_ position: Int) -> Int { return position < 0 ? 0 : (position > count ? count : position) }
-    
-    // ----------------------
-    // MARK: Format Checking
-    // ----------------------
-    public func isPurelyCharacterSet (_ characterSet: [Character]) -> Bool {
-        if count == 0 { return true }
-        for i in 0 ..< count { if !characterSet.contains(self[self.index(self.startIndex, offsetBy: i)]) { return false } }
-        return true
-    }
-    
-    public var isWholeNumber: Bool { return self.isPurelyCharacterSet(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) }
-    public var isInteger: Bool {
-        if count == 0 { return false }
-        if self[0] == "-" { return String(self.decapitated()).isWholeNumber }
-        else { return isWholeNumber }
-    }
-    public var isFloatingPointNumber: Bool {
-        let split = components(separatedBy: ".")
-        if split.count == 0 { return isInteger }
-        else if split.count == 1 { return split[0].isInteger }
-        else if split.count == 2 { return split[0].isInteger && split[1].isWholeNumber }
-        return false
-    }
-    public func isCurrencyAmount (currencySymbol: String) -> Bool {
-        return components(separatedBy: currencySymbol).joined().isFloatingPointNumber
-    }
-    public var isDollarAmount: Bool { return isCurrencyAmount(currencySymbol: "$") }
-    public var isVersionNumber: Bool {
-        for component in components(separatedBy: ".") { if !component.isWholeNumber { return false } }
-        return true
-    }
-    public var isValidEmailAddress: Bool {
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
-        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: self)
-    }
-    public var isHexCode: Bool {
-        if !(count == 4 || count == 7) { return false }
-        if self[0] != "#" { return false }
-        let code = self[1...].lowercased()
-        return String(code).isPurelyCharacterSet(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"])
-    }
-    
-    
-    
-    // ---------------
-    // MARK: Conversion
-    // ---------------
-    public var double: Double? {
-        if isFloatingPointNumber { return Double(self) }
-        if isDollarAmount { return Double(components(separatedBy: "$").joined()) }
-        return nil
-    }
-    public var float: Float? { if let double = double { return Float(double) } else { return nil } }
-    public var uiColor: UIColor? {
-        if !isHexCode { return nil }
-        return UIColor(hex: self)
-    }
-    public func attributed (with textStyle: TextStyle?) -> NSMutableAttributedString {
-        return NSMutableAttributedString(string: self, attributes: [NSAttributedStringKey.foregroundColor: (textStyle?.textColor ?? .black) as Any, NSAttributedStringKey.font: (textStyle?.font ?? UIFont()) as Any, NSAttributedStringKey.kern: textStyle?.characterSpacing ?? 1])
-    }
-    
-    
-    // ---------------
-    // MARK: Substring
-    // ---------------
-    public func decapitated () -> Substring {
-        if count == 0 { return "" }
-        return self[1...]
-    }
-    
-    public func removing (_ substring: String) -> String {
-        return self.components(separatedBy: substring).joined()
-    }
-    
-    // ---------------
-    // MARK: Ranges
-    // ---------------
-    public func rangeIsValid (_ nsRange: NSRange) -> Bool {
-        if nsRange.location == NSNotFound { return false }
-        if nsRange.location + nsRange.length > NSString(string: self).length { return false }
-        return true
-    }
-    
-    
-    // ------------------
-    // MARK: JSON Object
-    // ------------------
-    public var jsonObject: Any? {
-        guard let data = self.data(using: .utf8) else { return nil }
-        do { return try JSONSerialization.jsonObject(with: data, options: .init(rawValue: 0)) }
-        catch { return nil }
-    }
-    
-    
-    // --------------------
-    // MARK: Read/Write
-    // --------------------
-    public func writeTxtFileToDisk (_ fileName: String) {
-        guard let extensionlessFileName = fileName.components(separatedBy: ".txt").first else { return }
-        guard let data = self.data(using: String.Encoding.utf8) else { return }
-        let fileWrapper = FileWrapper(regularFileWithContents: data)
-        let filename = extensionlessFileName + ".txt"
-        fileWrapper.filename = filename
-        do {
-            let fileURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(filename)
-            try fileWrapper.write(to: fileURL, options: .atomic, originalContentsURL: nil)
-        } catch { print(error) }
-    }
-    
-    public static func readTxtFileFromBundle (_ fileName: String) -> String? {
-        guard let extensionlessFileName = fileName.components(separatedBy: ".txt").first else { return nil }
-        guard let filePath = Bundle.main.path(forResource: extensionlessFileName, ofType: "txt") else { return nil }
-        guard let data = NSData(contentsOfFile: filePath) as Data? else { return nil }
-        return String(data: data, encoding: .utf8)
-    }
-    
-    public static func readTxtFileFromDisk (_ fileName: String) -> String? {
-        guard let extensionlessFileName = fileName.components(separatedBy: ".txt").first else { return nil }
-        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(extensionlessFileName + ".txt")
-        guard let data = NSData(contentsOf: fileURL) as Data? else { return nil }
-        return String(data: data, encoding: .utf8)
-    }
+  // ----------------------
+  // MARK: Subscript
+  // ----------------------
+  subscript (index: Int) -> Substring? {
+    if !self.hasIndex(index) { return nil }
+    let stringIndex = self.index(self.startIndex, offsetBy: index)
+    return self[stringIndex ... stringIndex]
+  }
+  subscript (range: CountableRange<Int>) -> Substring {
+    let lowerBound = self.ensurePosition(range.lowerBound)
+    let upperBound = self.ensurePosition(range.upperBound)
+    return self[self.index(self.startIndex, offsetBy: lowerBound) ..< self.index(self.startIndex, offsetBy: upperBound)]
+  }
+  subscript (closedRange: CountableClosedRange<Int>) -> Substring {
+    var lowerBound = self.ensurePosition(closedRange.lowerBound)
+    var upperBound = self.ensurePosition(closedRange.upperBound)
+    if lowerBound == upperBound && lowerBound == count { return "" }
+    lowerBound = self.ensureIndex(lowerBound)
+    upperBound = self.ensureIndex(upperBound)
+    return self[self.index(self.startIndex, offsetBy: lowerBound) ... self.index(self.startIndex, offsetBy: upperBound)]
+  }
+  subscript (partialRangeFrom: CountablePartialRangeFrom<Int>) -> Substring {
+    if partialRangeFrom.lowerBound >= count { return "" }
+    let lowerBound = self.ensureIndex(partialRangeFrom.lowerBound)
+    return self[self.index(self.startIndex, offsetBy: lowerBound)...]
+  }
+  subscript (partialRangeUpTo: PartialRangeUpTo<Int>) -> Substring {
+    let upperBound = self.ensurePosition(partialRangeUpTo.upperBound)
+    return self[..<self.index(self.startIndex, offsetBy: upperBound)]
+  }
+  subscript (partialRangeThrough: PartialRangeThrough<Int>) -> Substring {
+    if partialRangeThrough.upperBound < 0 { return "" }
+    let upperBound = self.ensureIndex(partialRangeThrough.upperBound)
+    return self[...self.index(self.startIndex, offsetBy: upperBound)]
+  }
+  
+  // An index marks the locations of the characters, while a position marks the locations between characters
+  public func hasIndex (_ index: Int) -> Bool { return index == self.ensureIndex(index) }
+  public func ensureIndex (_ index: Int) -> Int { return index < 0 ? 0 : (index >= count ? count - 1 : index) }
+  public func hasPosition (_ position: Int) -> Bool { return position == self.ensurePosition(position) }
+  public func ensurePosition (_ position: Int) -> Int { return position < 0 ? 0 : (position > count ? count : position) }
+  
+  // ----------------------
+  // MARK: Format Checking
+  // ----------------------
+  public func isPurelyCharacterSet (_ characterSet: [Character]) -> Bool {
+    if count == 0 { return true }
+    for i in 0 ..< count { if !characterSet.contains(self[self.index(self.startIndex, offsetBy: i)]) { return false } }
+    return true
+  }
+  
+  public var isWholeNumber: Bool { return self.isPurelyCharacterSet(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) }
+  public var isInteger: Bool {
+    if count == 0 { return false }
+    if self[0] == "-" { return String(self.decapitated()).isWholeNumber }
+    else { return isWholeNumber }
+  }
+  public var isFloatingPointNumber: Bool {
+    let split = components(separatedBy: ".")
+    if split.count == 0 { return isInteger }
+    else if split.count == 1 { return split[0].isInteger }
+    else if split.count == 2 { return split[0].isInteger && split[1].isWholeNumber }
+    return false
+  }
+  public func isCurrencyAmount (currencySymbol: String) -> Bool {
+    return components(separatedBy: currencySymbol).joined().isFloatingPointNumber
+  }
+  public var isDollarAmount: Bool { return isCurrencyAmount(currencySymbol: "$") }
+  public var isVersionNumber: Bool {
+    for component in components(separatedBy: ".") { if !component.isWholeNumber { return false } }
+    return true
+  }
+  public var isValidEmailAddress: Bool {
+    let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+    return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: self)
+  }
+  public var isHexCode: Bool {
+    if !(count == 4 || count == 7) { return false }
+    if self[0] != "#" { return false }
+    let code = self[1...].lowercased()
+    return String(code).isPurelyCharacterSet(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"])
+  }
+  
+  
+  
+  // ---------------
+  // MARK: Conversion
+  // ---------------
+  public var double: Double? {
+    if isFloatingPointNumber { return Double(self) }
+    if isDollarAmount { return Double(components(separatedBy: "$").joined()) }
+    return nil
+  }
+  public var float: Float? { if let double = double { return Float(double) } else { return nil } }
+  public var uiColor: UIColor? {
+    if !isHexCode { return nil }
+    return UIColor(hex: self)
+  }
+  public func attributed (with textStyle: TextStyle?) -> NSMutableAttributedString {
+    return NSMutableAttributedString(string: self, attributes: [NSAttributedStringKey.foregroundColor: (textStyle?.textColor ?? .black) as Any, NSAttributedStringKey.font: (textStyle?.font ?? UIFont()) as Any, NSAttributedStringKey.kern: textStyle?.characterSpacing ?? 1])
+  }
+  
+  
+  // ---------------
+  // MARK: Substring
+  // ---------------
+  public func decapitated () -> Substring {
+    if count == 0 { return "" }
+    return self[1...]
+  }
+  
+  public func removing (_ substring: String) -> String {
+    return self.components(separatedBy: substring).joined()
+  }
+  
+  // ---------------
+  // MARK: Ranges
+  // ---------------
+  public func rangeIsValid (_ nsRange: NSRange) -> Bool {
+    if nsRange.location == NSNotFound { return false }
+    if nsRange.location + nsRange.length > NSString(string: self).length { return false }
+    return true
+  }
+  
+  
+  // ------------------
+  // MARK: JSON Object
+  // ------------------
+  public var jsonObject: Any? {
+    guard let data = self.data(using: .utf8) else { return nil }
+    do { return try JSONSerialization.jsonObject(with: data, options: .init(rawValue: 0)) }
+    catch { return nil }
+  }
+  
+  
+  // --------------------
+  // MARK: Read/Write
+  // --------------------
+  public func writeTxtFileToDisk (_ filename: String) {
+    guard let extensionlessFilename = filename.components(separatedBy: ".txt").first else { return }
+    guard let data = self.data(using: String.Encoding.utf8) else { return }
+    let filename = extensionlessFilename + ".txt"
+    data.writeToDisk(filename: filename)
+  }
+  
+  public static func readTxtFileFromBundle (_ fileName: String) -> String? {
+    guard let extensionlessFileName = fileName.components(separatedBy: ".txt").first else { return nil }
+    guard let filePath = Bundle.main.path(forResource: extensionlessFileName, ofType: "txt") else { return nil }
+    guard let data = NSData(contentsOfFile: filePath) as Data? else { return nil }
+    return String(data: data, encoding: .utf8)
+  }
+  
+  public static func readTxtFileFromDisk (_ fileName: String) -> String? {
+    guard let extensionlessFileName = fileName.components(separatedBy: ".txt").first else { return nil }
+    let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(extensionlessFileName + ".txt")
+    guard let data = NSData(contentsOf: fileURL) as Data? else { return nil }
+    return String(data: data, encoding: .utf8)
+  }
     
     
 }
--- JABSwiftCore/UIImageExtension.swift
+++ JABSwiftCore/UIImageExtension.swift
@@ -10,60 +10,98 @@ import Foundation
 import UIKit
 
 
-public extension UIImage {
-    
-    
-    
-    // MARK: Coloring
-    public func imageTintedWithColor(_ color: UIColor?, fraction: CGFloat = 0) -> UIImage {
-        
-        if color == nil { return self }
-        // Construct new image the same size as this one.
-        var image = UIImage()
-        if UIScreen.instancesRespond(to: #selector(NSDecimalNumberBehaviors.scale)) {
-            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-        } else {
-            UIGraphicsBeginImageContext(size)
-        }
-        
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
-        // Composite tint color at its own opacity.
-        color!.set()
-        UIRectFill(rect)
-        
-        // Mask tint color-swatch to this image's opaque mask.
-        // We want behaviour like NSCompositeDestinationIn on Mac OS X.
-        draw(in: rect, blendMode: CGBlendMode.destinationIn, alpha: 1.0)
-        
-        // Finally, composite this image over the tinted mask at desired opacity.
-        if fraction > 0.0 {
-            draw(in: rect, blendMode: CGBlendMode.sourceAtop, alpha: fraction)
-        }
-        image = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        return image
-        
+extension UIImage {
+  public func imageTintedWithColor(_ color: UIColor?, fraction: CGFloat = 0) -> UIImage {
+    if color == nil { return self }
+    // Construct new image the same size as this one.
+    var image = UIImage()
+    if UIScreen.instancesRespond(to: #selector(NSDecimalNumberBehaviors.scale)) {
+      UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+    } else {
+      UIGraphicsBeginImageContext(size)
     }
     
+    let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
     
+    // Composite tint color at its own opacity.
+    color!.set()
+    UIRectFill(rect)
     
-    // MARK: Resizing
+    // Mask tint color-swatch to this image's opaque mask.
+    // We want behaviour like NSCompositeDestinationIn on Mac OS X.
+    draw(in: rect, blendMode: CGBlendMode.destinationIn, alpha: 1.0)
     
-    func resized(withPercentage percentage: CGFloat) -> UIImage? {
-        let canvasSize = CGSize(width: size.width * percentage, height: size.height * percentage)
-        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
-        defer { UIGraphicsEndImageContext() }
-        draw(in: CGRect(origin: .zero, size: canvasSize))
-        return UIGraphicsGetImageFromCurrentImageContext()
+    // Finally, composite this image over the tinted mask at desired opacity.
+    if fraction > 0.0 {
+      draw(in: rect, blendMode: CGBlendMode.sourceAtop, alpha: fraction)
     }
-    func resized(toWidth width: CGFloat) -> UIImage? {
-        let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
-        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
-        defer { UIGraphicsEndImageContext() }
-        draw(in: CGRect(origin: .zero, size: canvasSize))
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
+    image = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
     
+    return image
+  }
+}
+
+
+extension UIImage {
+  public func resized(withPercentage percentage: CGFloat) -> UIImage? {
+    let canvasSize = CGSize(width: size.width * percentage, height: size.height * percentage)
+    UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+    defer { UIGraphicsEndImageContext() }
+    draw(in: CGRect(origin: .zero, size: canvasSize))
+    return UIGraphicsGetImageFromCurrentImageContext()
+  }
+  public func resized(toWidth width: CGFloat) -> UIImage? {
+    let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+    UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+    defer { UIGraphicsEndImageContext() }
+    draw(in: CGRect(origin: .zero, size: canvasSize))
+    return UIGraphicsGetImageFromCurrentImageContext()
+  }
+}
+
+
+extension UIImage {
+  public func writePNGFileToDisk (_ filename: String) {
+    guard let extensionlessFilename = filename.components(separatedBy: ".png").first else { return }
+    guard let data = UIImagePNGRepresentation(self) else { return }
+    let filename = extensionlessFilename + ".png"
+    data.writeToDisk(filename: filename)
+  }
+  
+  public static func readPNGFileFromBundle (_ filename: String) -> UIImage? {
+    guard let extensionlessFilename = filename.components(separatedBy: ".png").first else { return nil }
+    guard let filePath = Bundle.main.path(forResource: extensionlessFilename, ofType: "png") else { return nil }
+    guard let data = NSData(contentsOfFile: filePath) as Data? else { return nil }
+    return UIImage(data: data)
+  }
+  
+  public static func readPNGFileFromDisk (_ filename: String) -> UIImage? {
+    guard let extensionlessFilename = filename.components(separatedBy: ".png").first else { return nil }
+    let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(extensionlessFilename + ".png")
+    guard let data = NSData(contentsOf: fileURL) as Data? else { return nil }
+    return UIImage(data: data)
+  }
+  
+  
+  public func writeJPGFileToDisk (_ filename: String) {
+    guard let extensionlessFilename = filename.components(separatedBy: ".jpg").first else { return }
+    guard let data = UIImageJPEGRepresentation(self, 1.0) else { return }
+    let filename = extensionlessFilename + ".jpg"
+    data.writeToDisk(filename: filename)
+  }
+  
+  public static func readJPGFileFromBundle (_ filename: String) -> UIImage? {
+    guard let extensionlessFilename = filename.components(separatedBy: ".jpg").first else { return nil }
+    guard let filePath = Bundle.main.path(forResource: extensionlessFilename, ofType: "jpg") else { return nil }
+    guard let data = NSData(contentsOfFile: filePath) as Data? else { return nil }
+    return UIImage(data: data)
+  }
+  
+  public static func readJPGFileFromDisk (_ filename: String) -> UIImage? {
+    guard let extensionlessFilename = filename.components(separatedBy: ".jpg").first else { return nil }
+    let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(extensionlessFilename + ".jpg")
+    guard let data = NSData(contentsOf: fileURL) as Data? else { return nil }
+    return UIImage(data: data)
+  }
 }
