678292cb4300b1439598bb9cad921ba62da0e837
jeremyabannister <jeremy.a.bannister@gmail.com>
2018-05-19 04:20:18 -0700

    Added CGColorExtension.swift

---
+++ JABSwiftCore/CGColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  CGColorExtension.swift
+//  JABSwiftCore
+//
+//  Created by Jeremy Bannister on 5/19/18.
+//  Copyright © 2018 Jeremy Bannister. All rights reserved.
+//
+
+import UIKit
+
+public extension CGColor {
+  public static var clear: CGColor { return UIColor.clear.cgColor }
+}
