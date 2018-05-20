ac3a9b25e8c11144725d2a75562d5dc14e833431
jeremyabannister <jeremy.a.bannister@gmail.com>
2018-05-19 23:06:57 -0700

    Added red, blue, green and yellow shortcuts to CGColor

--- JABSwiftCore/CGColorExtension.swift
+++ JABSwiftCore/CGColorExtension.swift
@@ -10,4 +10,8 @@ import UIKit
 
 public extension CGColor {
   public static var clear: CGColor { return UIColor.clear.cgColor }
+  public static var red: CGColor { return UIColor.red.cgColor }
+  public static var blue: CGColor { return UIColor.blue.cgColor }
+  public static var green: CGColor { return UIColor.green.cgColor }
+  public static var yellow: CGColor { return UIColor.yellow.cgColor }
 }
